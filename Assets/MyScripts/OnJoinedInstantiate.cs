758c1d4bc5629e34e80049150cd7a8401f5f108d
Anton Konovalov <konovalov.anton.39@gmail.com>
2017-01-15 18:22:35 +0300

    fps controller made, PUN added with demos

---
+++ Assets/MyScripts/OnJoinedInstantiate.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+
+public class OnJoinedInstantiate : MonoBehaviour
+{
+    public Transform SpawnPosition1;
+    public Transform SpawnPosition2;
+    public GameObject[] PrefabsToInstantiate;
+
+    public void OnJoinedRoom()
+    {
+        if (this.PrefabsToInstantiate != null)
+        {
+            foreach (GameObject o in this.PrefabsToInstantiate)
+            {
+                Debug.Log("Instantiating: " + o.name);
+
+                Vector3 spawnPos = Vector3.up;
+                if (this.SpawnPosition1 != null && PhotonNetwork.playerList.Length % 2 == 0)
+                {
+                    spawnPos = this.SpawnPosition1.position;
+                }
+                else if(this.SpawnPosition2 != null && PhotonNetwork.playerList.Length % 2 == 1)
+                {
+                    spawnPos = this.SpawnPosition2.position;
+                }
+                PhotonNetwork.Instantiate(o.name, spawnPos, Quaternion.identity, 0);
+            }
+        }
+    }
+}
