e8cdd21eb21971d0b7999c47e70e7981d334f52b
Anton Konovalov <konovalov.anton.39@gmail.com>
2017-04-09 17:01:34 +0300

    logs first steps

---
+++ Assets/MyScripts/GlobalLogSaver.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class GlobalLogSaver : MonoBehaviour{
+
+    static GlobalLogSaver _instance;
+	public static GlobalLogSaver Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = new GlobalLogSaver();
+            }
+            return _instance;
+        }
+    }
+
+    List<string> logs = new List<string>();
+    StreamWriter sr;
+
+
+    public void AddLogString(string log)
+    {
+        logs.Add(log);
+    }
+
+    private void Awake()
+    {
+        if(_instance == null)
+        {
+            _instance = this;
+        }
+        sr = File.CreateText(@"C:\Users\konov_000\Desktop\git thesis\CoOpMaze\Assets\save.txt");        
+    }
+
+    private void Update()
+    {
+        if(logs.Count > 0)
+        {
+            foreach (var item in logs)
+            {
+                sr.WriteLine(item);
+            }
+            logs.Clear();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        sr.Close();
+    }
+
+
+}
---
+++ Assets/MyScripts/TransformSaver.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformSaver : MonoBehaviour {
+
+    float interval = 0.1f;
+    float t = 0;
+
+	// Use this for initialization
+	void Start () {
+        PlayerBirth b = new PlayerBirth()
+        {
+            id = gameObject.GetInstanceID()
+        };
+        PlayerTransform p = new PlayerTransform()
+        {
+            id = gameObject.GetInstanceID(),
+            pos = transform.position,
+            rot = transform.rotation.eulerAngles,
+            scale = transform.localScale
+        };
+        string log = JsonUtility.ToJson(b);
+        GlobalLogSaver.Instance.AddLogString(log);
+        log = JsonUtility.ToJson(p);
+        GlobalLogSaver.Instance.AddLogString(log);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        t += Time.deltaTime;
+        if(t > interval)
+        {
+            PlayerTransform p = new PlayerTransform()
+            {
+                id = gameObject.GetInstanceID(),
+                pos = transform.position,
+                rot = transform.rotation.eulerAngles,
+                scale = transform.localScale
+            };
+            string log = JsonUtility.ToJson(p);
+            GlobalLogSaver.Instance.AddLogString(log);
+            t = 0;
+        }
+	}
+}
+
+class LogObject
+{
+    public float timeStamp;
+    public string LogType;
+    public LogObject(string type)
+    {
+        timeStamp = Time.time;
+        LogType = type;
+    }
+}
+
+class PlayerTransform : LogObject
+{
+    public Vector3 pos;
+    public Vector3 rot;
+    public Vector3 scale;
+    public int id;
+    public PlayerTransform() : base("PlayerTransform") {}
+}
+
+class PlayerBirth : LogObject
+{
+    public int id;
+    public PlayerBirth() : base("PlayerBirth") { }
+}
